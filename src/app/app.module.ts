41c2acf75ca2eae9156b39b89d17d242cc9c7998
Arpit Santoki <santokiarpit0@gmail.com>
2024-02-11 16:47:31 +0530

    change in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/account/login/login.component';
-import { FullCalendarModule } from '@fullcalendar/angular';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { LayoutModule } from './module/layout/layout.module';
 import { firebaseProviders } from './core/firebase.config';
 import { AccountModule } from './module/account/account.module';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -26,7 +23,7 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     LayoutModule,
     AccountModule
   ],
-  providers: [firebaseProviders, { provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [firebaseProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
